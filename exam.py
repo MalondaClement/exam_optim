615ab3f5c63bf4bb709d2ad840bf8a0ed2560a0b
MalondaClement <46990837+MalondaClement@users.noreply.github.com>
2022-06-28 17:28:28 +0200

    ajout dim 1

--- exam.py
+++ exam.py
@@ -26,11 +26,55 @@ def gd2_momentum(x, grad, alpha, beta=0.9, max_iter=10):
     return xs
 
 def main():
-    ## Dim 1
+    lr = 0.7
 
     ## Dim 1
+    print("Dim 1")
+    plt.figure(1)
+
+    #calcul de la solution
+    w0 = np.array([20])
+    res_1 = gd2_momentum(w0, grad, lr, max_iter=33)
+
+    #plot
+    x = np.linspace(-20, 20, 100)
+    y = np.zeros(x.shape)
+    for i in range(x.shape[0]):
+        y[i] = f(np.array([x[i]]))
+    res_1_y = np.zeros(res_1.shape)
+    for i in range(res_1.shape[0]):
+        res_1_y[i] = f(np.array([res_1[i]]))
+
+    plt.plot(x,y)
+    plt.plot(res_1, res_1_y, 'o-', c='red')
+
+    print("w final: {}".format(res_1[-1]))
+    print("")
 
     ## Dim 2
+    plt.figure(2)
+    w0 = np.array([-20, 20])
+    res_2 = gd2_momentum(w0, grad, lr, max_iter=33)
+
+    #plot
+    # x = np.linspace(-20, 20, 100)
+    # y = np.linspace(-20, 20, 100)
+    # X, Y = np.meshgrid(x, y)
+    # z = np.zeros((x.shape[0], y.shape[0]))
+    # for i in range(x.shape[0]):
+    #     for j in range(y.shape[0]):
+    #         z[i, j] = f(np.array([x[i], y[j]]))
+    # levels = [0.1,1,2,4,9, 16, 25, 36, 49, 64, 81, 100, 1000]
+    # c = plt.contour(X, Y, z, levels)
+
+    print("")
+
+    ## Dim 10
+    plt.figure(3)
+    w0 = np.array([20, 20, -20, 20, -20, 20, 20, 20, 20, 20])
+    # res_10 = gd2_momentum(w0, grad, lr, max_iter=33)
+
+    plt.show()
 
 if __name__ == "__main__":
     main()
